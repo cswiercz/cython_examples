3eb2e979eab9956423531fa37195838a87c5d06f
Chris Swierczewski <cswiercz@gmail.com>
2015-03-23 10:27:37 -0700

    Update module name in runtests

--- memoryview_pointers/runtests.py
+++ memoryview_pointers/runtests.py
@@ -1,6 +1,6 @@
 import numpy
 
-from cython_tests.matvec import matvec
+from memoryview_pointers.matvec import matvec
 
 A = numpy.array([[1,2,3],[4,5,6],[7,8,9]], dtype=numpy.double)
 x = numpy.array([0.5,0.2,0.1], dtype=numpy.double)
